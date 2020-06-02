d854bc3d05ffeaec12e3676c476e19a67216cd47
Jeff Kerr <jeff@casefleet.com>
2020-06-01 21:28:39 -0400

    update many things

--- test/test.js
+++ test/test.js
@@ -2,11 +2,11 @@ import { toHtml, toPlain } from "../lib";
 import assert from "assert";
 import fs from "fs";
 
-describe("Test DumbDown", function() {
+describe("Test DumbDown", function () {
   let content = fs.readFileSync("test/test_examples.json");
   let data = JSON.parse(content);
-  data.forEach(example => {
-    it(`gets "${example["md"]}" right`, function() {
+  data.forEach((example) => {
+    it(`gets "${example["md"]}" right`, function () {
       assert.equal(toHtml(example["md"]), example["html"]);
       assert.equal(toPlain(example["md"]), example["plain"]);
     });
